855699d04309c8c72a55e882c59d5be15edbfc87
dinozindev <pugplayzcraft@gmail.com>
2024-04-19 13:32:07 -0300

    vetores

--- src/aula11/Exemplo2Vetor.java
+++ src/aula11/Exemplo2Vetor.java
@@ -39,4 +39,47 @@ public class Exemplo2Vetor {
 			System.out.printf("Vetor[%d]: %d\n", i, vetor[i]);
 		}
 	}
+	
+	public int obterNumero() {
+		System.out.println("*-* Obter um número *-*");
+		input = new Scanner(System.in);
+		System.out.println("Número: ");
+		int n = input.nextInt();
+		return n;
+	}
+	
+	public int verificarOcorrencias(int n, int[] vetor) {
+		System.out.println("*-- Verificar Ocorrencias de um numero --*");
+		int cont = 0;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			if (n == vetor[i]) {
+				cont++;
+			}
+		}
+		return cont;
+	}
+	
+	public int[] verificarParesImpares(int [] vetor) {
+		System.out.println("*-- Verificar Ocorrencia de pares e impares");
+		int contPares = 0;
+		int contImpares = 0;
+		int vetorValores[] = new int[2];
+		
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] % 2 == 0) {
+				contPares++;
+			} else {
+				contImpares++;
+			}
+		}
+		
+		vetorValores[0] = contPares;
+		vetorValores[1] = contImpares;
+		return vetorValores;
+	}
+	
+	// FAZER PROXIMA AULA
+	// escrever um metodo para verificar os numeros pares e impares presentes no vetor.
+	// retorne quantos elementos pares e quantos elementos impares
 }
--- src/aula11/TesteVetor.java
+++ src/aula11/TesteVetor.java
@@ -4,6 +4,18 @@ public class TesteVetor {
 	public static void main(String[] args) {
 		
 		Exemplo2Vetor v2 = new Exemplo2Vetor(); 
-		v2.imprimirVetor(v2.atribuiVetor(v2.criarVetor(v2.tamanhoVetor())));	
+		int t = v2.tamanhoVetor();
+		
+		int vetor[] = v2.criarVetor(t);
+		v2.imprimirVetor(vetor);
+		
+		vetor = v2.atribuiVetor(vetor);
+		v2.imprimirVetor(vetor);
+		
+		int n = v2.obterNumero();
+		System.out.printf("Ocorrencias do numero %d: %d \n", n, v2.verificarOcorrencias(n, vetor));
+		
+		int vetorParesImpares[] = v2.verificarParesImpares(vetor);
+		System.out.printf("Ocorrencias de pares: %d\nOcorrencia de impares: %d\n", vetorParesImpares[0], vetorParesImpares[1]);
 	}
 }
