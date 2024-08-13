50235cc01b185205d511f34c0097c91bffb1036f
Lucas <124621971+dinozindev@users.noreply.github.com>
2024-08-13 11:51:56 -0300

    aula 21

--- src/aula20_Polimorfismo/TesteContas.java
+++ src/aula20_Polimorfismo/TesteContas.java
@@ -2,6 +2,31 @@ package aula20_Polimorfismo;
 
 public class TesteContas {
 	public static void main(String[] args) {
+		ContaCorrente cc1 = new ContaCorrente();
+		System.out.println("Saldo: R$" + cc1.getSaldo());
+		cc1.depositar(3000);
+		System.out.println("Saldo: R$" + cc1.getSaldo());
+		cc1.sacar(500);
+		System.out.println("Saldo: R$" + cc1.getSaldo());
+		
+		System.out.println("\n=============================\n");
+		
+		ContaPoupanca cp1 = new ContaPoupanca();
+		System.out.println("Saldo: R$" + cp1.getSaldo());
+		cp1.depositar(1000);
+		System.out.println("Saldo: R$" + cp1.getSaldo());
+		cp1.sacar(500);
+		System.out.println("Saldo: R$" + cp1.getSaldo());
+		
+		System.out.println("\n=============================\n");
+		
+		GeradorExtratos ge1 = new GeradorExtratos();
+		System.out.println("Polimorfismo");
+		ge1.geradorConta(cp1);
+		ge1.geradorConta(cc1);
+		
+		cp1.imprimirExtrato();
+		cc1.imprimirExtrato();
 		
 	}
 }
